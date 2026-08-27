3660a1ae8ff0997fca3e37792886fde7c889b68b
Parveen Kumar <parveenyadavpy57377@gmail.com>
2026-08-27 17:48:01 +0530

    Initial Push

--- ddr_portal/ddr_portal/urls.py
+++ ddr_portal/ddr_portal/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for ddr_portal project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+   path('admin/', admin.site.urls),
+    path('', include('Users.urls')),
 ]
